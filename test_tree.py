111b40f74deac6a26d613a1a81cfe6c6c990af8e
cole.koester <cole.koester@imanage.com>
2020-09-07 10:44:21 -0500

    testing

--- test_tree.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from suffix_tree import SuffixTree
-
-def test_build_tree():
-    x = SuffixTree('abcd')
-    print(x)
---
+++ tests/test_tree.py
@@ -0,0 +1,6 @@
+from src.suffix_tree import SuffixTree
+
+
+def test_build_tree():
+    x = SuffixTree('abcd')
+    print(x)
